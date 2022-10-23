c1f407f281f97012f28ffe80f035dbb4e8fe1959
faizan-99 <mfaizy605@gmail.com>
2022-10-23 22:18:21 +0500

    create CurReportCard component

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import SideDrawer from '../components/miscellaneous/SideDrawer'
-
-
+import React from "react";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import CurrReportsCard from "../components/CurrReportsCard.js";
+import { Box } from "@chakra-ui/react";
 
 const Homepage = () => {
-    return (
-        <div style={{ width: "100%" }}>
-            <SideDrawer />
-        </div>
-    )
-}
+  return (
+    <div style={{ width: "100%" }}>
+      <SideDrawer />
+      <CurrReportsCard />
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
---
+++ src/components/CurrReportsCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CurrReportsCard = () => {
+  return <div>CurrReportsCard</div>;
+};
+
+export default CurrReportsCard;
