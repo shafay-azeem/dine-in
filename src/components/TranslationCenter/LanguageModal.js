facb3241dca8b960260c835203a2d062f682aba0
faizan-99 <mfaizy605@gmail.com>
2022-10-28 20:31:30 +0500

    updated

--- src/components/TranslationCenter/LanguageModal.js
+++ src/components/TranslationCenter/LanguageModal.js
@@ -1,39 +1,50 @@
-import React from 'react'
+import React from "react";
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-} from '@chakra-ui/react'
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Select,
+} from "@chakra-ui/react";
 
 const LanguageModal = (props) => {
-    return (
-        <>
+  return (
+    <>
+      <Modal isOpen={props.isOpen} onClose={props.onClose} size="xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Choose languages</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Select placeholder="Select languages">
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </Select>
+          </ModalBody>
 
+          <ModalFooter>
+            <Button
+              variant="outline"
+              colorScheme="blue"
+              mr={3}
+              size="md"
+              onClick={props.onClose}
+            >
+              Cancel
+            </Button>
+            <Button variant="solid" size="md" colorScheme="blue">
+              Save
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
 
-            <Modal isOpen={props.isOpen} onClose={props.onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Modal Title</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        {/* <Lorem count={2} /> */}
-                    </ModalBody>
-
-                    <ModalFooter>
-                        <Button colorScheme='blue' mr={3} onClick={props.onClose}>
-                            Close
-                        </Button>
-                        <Button variant='ghost'>Secondary Action</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </>
-    )
-}
-
-export default LanguageModal
+export default LanguageModal;
