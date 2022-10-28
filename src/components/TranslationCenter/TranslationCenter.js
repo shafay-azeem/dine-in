81bb9cfb64a3f76a1287e9c6646c75b476295353
faizan-99 <mfaizy605@gmail.com>
2022-10-28 20:09:00 +0500

    updated

--- src/components/TranslationCenter/TranslationCenter.js
+++ src/components/TranslationCenter/TranslationCenter.js
@@ -1,7 +1,91 @@
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Grid,
+  GridItem,
+  Text,
+  Box,
+  VStack,
+  Link,
+} from "@chakra-ui/react";
 import React from "react";
 
 const TranslationCenter = () => {
-  return <div>TranslationCenter</div>;
+  return (
+    <>
+      <Grid>
+        <GridItem w="100%" bg="white" height="120%">
+          <Text ml="10" fontWeight="500" fontSize="25" mt={5}>
+            Translation Center
+          </Text>
+        </GridItem>
+      </Grid>
+
+      <Box ml="10" mt={5}>
+        <Tabs>
+          <TabList>
+            <Tab>Menus</Tab>
+            <Tab>Modifiers</Tab>
+            <Tab>Surveys</Tab>
+            <Tab>Others</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>three!</p>
+            </TabPanel>
+            <TabPanel>
+              <Box bg="white" w="100%" p={4}>
+                <Tabs variant="soft-rounded" colorScheme="green">
+                  <TabList>
+                    <Tab>Info Page</Tab>
+                    <Tab>Service Options</Tab>
+                    <Tab>Menu Button</Tab>
+                    <Tab>Venue Name</Tab>
+                    <Tab>Welcome Message</Tab>
+                    <Tab>Restaurant Notes</Tab>
+                  </TabList>
+                  <TabPanels>
+                    <TabPanel>
+                      <Text fontSize="md">
+                        Please select the languages you want to translate into.
+                        <Link color="blue">Click </Link>here to add a new
+                        language.
+                      </Text>
+                    </TabPanel>
+                    <TabPanel>
+                      <p>Service Options</p>
+                    </TabPanel>
+                    <TabPanel>
+                      <p>Menu Button</p>
+                    </TabPanel>
+                    <TabPanel>
+                      <p>Venue Name</p>
+                    </TabPanel>
+                    <TabPanel>
+                      <p>Welcome Message</p>
+                    </TabPanel>
+                    <TabPanel>
+                      <p>Restaurant Notes</p>
+                    </TabPanel>
+                  </TabPanels>
+                </Tabs>
+              </Box>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </>
+  );
 };
 
 export default TranslationCenter;
