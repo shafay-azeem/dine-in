049a39c55866fe759ab31f753174aa9159dd15b1
faizan-99 <mfaizy605@gmail.com>
2022-10-28 16:36:39 +0500

    updated

--- src/components/MenuManagement/PurchaseModal.js
+++ src/components/MenuManagement/PurchaseModal.js
@@ -1,35 +1,105 @@
-import React from 'react'
+import React from "react";
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  RadioGroup,
+  HStack,
+  Radio,
+  FormHelperText,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
 } from "@chakra-ui/react";
 
 const PurchaseModal = (props) => {
-    return (
-        <>
-            <Modal isOpen={props.isOpen} onClose={props.onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Create your account</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={6}></ModalBody>
+  return (
+    <>
+      <Modal isOpen={props.isOpen} onClose={props.onClose} size="4xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add a Promotion</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl isRequired>
+              <FormLabel fontWeight="400">Promotion Name</FormLabel>
+              <Input placeholder="New Year Promotion" />
+            </FormControl>
 
-                    <ModalFooter>
-                        <Button colorScheme="blue" mr={3}>
-                            Save
-                        </Button>
-                        <Button onClick={props.onClose}>Cancel</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </>
-    )
-}
+            <FormControl isRequired mt={5}>
+              <FormLabel fontWeight="400">Description</FormLabel>
+              <Input placeholder="10% Discount" />
+            </FormControl>
 
-export default PurchaseModal
+            <FormControl mt={5}>
+              <FormLabel fontWeight="400">Favorite Naruto Character</FormLabel>
+              <RadioGroup defaultValue="Itachi">
+                <HStack spacing="24px">
+                  <Radio value="Time Based">Time Based</Radio>
+                  <Radio value="Selection Based">Selection Based</Radio>
+                  <Radio value="Banner Based">Banner Based</Radio>
+                </HStack>
+                {/* <FormHelperText>
+                  Promotion is displayed after a certain time.
+                </FormHelperText> */}
+              </RadioGroup>
+            </FormControl>
+
+            <FormControl mt={5}>
+              <FormLabel fontWeight="400">Display After</FormLabel>
+              <NumberInput size="sm" maxW={24} defaultValue={15} min={2}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+
+            <FormControl mt={5}>
+              <FormLabel fontWeight="400">Promotion Close</FormLabel>
+              <RadioGroup defaultValue="Itachi">
+                <HStack spacing="24px">
+                  <Radio value="1">
+                    Keep the promotion open until the user closes.
+                  </Radio>
+                  <Radio value="2">Close promotion after x seconds.</Radio>
+                </HStack>
+              </RadioGroup>
+            </FormControl>
+
+            <FormControl mt={5}>
+              <FormLabel fontWeight="400">Promotion Duration</FormLabel>
+              <NumberInput size="sm" maxW={24} defaultValue={15} min={2}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Save
+            </Button>
+            <Button onClick={props.onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default PurchaseModal;
