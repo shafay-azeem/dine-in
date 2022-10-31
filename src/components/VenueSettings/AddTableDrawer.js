bc25a4b05ac8f9c5559870223a394e901b597cbc
faizan-99 <mfaizy605@gmail.com>
2022-10-31 14:13:46 +0500

    updated add table drawer

--- src/components/VenueSettings/AddTableDrawer.js
+++ src/components/VenueSettings/AddTableDrawer.js
@@ -1,73 +1,98 @@
-import { AddIcon } from '@chakra-ui/icons'
-import { Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, FormLabel, Input, InputGroup, InputLeftAddon, InputRightAddon, Select, Stack, Textarea } from '@chakra-ui/react'
-import React from 'react'
+import { AddIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Radio,
+  RadioGroup,
+  Select,
+  Stack,
+  Textarea,
+} from "@chakra-ui/react";
+import React from "react";
 
 const AddTableDrawer = (props) => {
-    return (
-        <>
+  return (
+    <>
+      <Drawer
+        isOpen={props.isOpen}
+        placement="right"
+        onClose={props.onClose}
+        size="md"
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth="1px">Add New Tables</DrawerHeader>
 
-            <Drawer
-                isOpen={props.isOpen}
-                placement='right'
-                // initialFocusRef={firstField}
-                onClose={props.onClose}
-            >
-                <DrawerOverlay />
-                <DrawerContent>
-                    <DrawerCloseButton />
-                    <DrawerHeader borderBottomWidth='1px'>
-                        Create a new account
-                    </DrawerHeader>
+          <DrawerBody>
+            <Stack spacing="24px">
+              <Box m={6}>
+                <FormControl mt={5}>
+                  <FormLabel>
+                    How would you like to create new tables?
+                  </FormLabel>
+                  <RadioGroup defaultValue="Itachi">
+                    <HStack spacing="24px">
+                      <Radio value="Automatic Table Names">
+                        Automatic Table Names
+                      </Radio>
+                      <Radio value="Custom Table Names">
+                        Custom Table Names
+                      </Radio>
+                    </HStack>
+                  </RadioGroup>
+                </FormControl>
 
-                    <DrawerBody>
-                        <Stack spacing='24px'>
-                            <Box>
-                                <FormLabel htmlFor='username'>Name</FormLabel>
-                                <Input
-                                    // ref={firstField}
-                                    id='username'
-                                    placeholder='Please enter user name'
-                                />
-                            </Box>
+                <FormControl mt={5}>
+                  <FormLabel fontWeight="400">Label (Suffix):</FormLabel>
+                  <Input placeholder="" size="sm" borderRadius="8px" />
+                </FormControl>
 
-                            <Box>
-                                <FormLabel htmlFor='url'>Url</FormLabel>
-                                <InputGroup>
-                                    <InputLeftAddon>http://</InputLeftAddon>
-                                    <Input
-                                        type='url'
-                                        id='url'
-                                        placeholder='Please enter domain'
-                                    />
-                                    <InputRightAddon>.com</InputRightAddon>
-                                </InputGroup>
-                            </Box>
+                <FormControl mt={5}>
+                  <FormLabel fontWeight="400">Number of Tables:</FormLabel>
+                  <NumberInput min={1} size="sm">
+                    <NumberInputField borderRadius="8px" />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </FormControl>
 
-                            <Box>
-                                <FormLabel htmlFor='owner'>Select Owner</FormLabel>
-                                <Select id='owner' defaultValue='segun'>
-                                    <option value='segun'>Segun Adebayo</option>
-                                    <option value='kola'>Kola Tioluwani</option>
-                                </Select>
-                            </Box>
+                <FormControl mt={3}>
+                  <FormLabel fontWeight="400">Starting from:</FormLabel>
+                  <Input placeholder="1" size="sm" borderRadius="8px" />
+                </FormControl>
+              </Box>
+            </Stack>
+          </DrawerBody>
 
-                            <Box>
-                                <FormLabel htmlFor='desc'>Description</FormLabel>
-                                <Textarea id='desc' />
-                            </Box>
-                        </Stack>
-                    </DrawerBody>
+          <DrawerFooter borderTopWidth="1px">
+            <Button variant="outline" mr={3} onClick={props.onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Create Tables</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
 
-                    <DrawerFooter borderTopWidth='1px'>
-                        <Button variant='outline' mr={3} onClick={props.onClose}>
-                            Cancel
-                        </Button>
-                        <Button colorScheme='blue'>Submit</Button>
-                    </DrawerFooter>
-                </DrawerContent>
-            </Drawer>
-        </>
-    )
-}
-
-export default AddTableDrawer
+export default AddTableDrawer;
