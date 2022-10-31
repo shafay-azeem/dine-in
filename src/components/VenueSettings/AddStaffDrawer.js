6dc147460f6d634e375df7b6f21889a0bca790c0
faizan-99 <mfaizy605@gmail.com>
2022-10-31 14:43:05 +0500

    updated

--- src/components/VenueSettings/AddStaffDrawer.js
+++ src/components/VenueSettings/AddStaffDrawer.js
@@ -1,98 +1,105 @@
-import { AddIcon } from "@chakra-ui/icons";
 import {
-    Box,
-    Button,
-    Drawer,
-    DrawerBody,
-    DrawerCloseButton,
-    DrawerContent,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    FormControl,
-    FormLabel,
-    HStack,
-    Input,
-    NumberDecrementStepper,
-    NumberIncrementStepper,
-    NumberInput,
-    NumberInputField,
-    NumberInputStepper,
-    Radio,
-    RadioGroup,
-    Select,
-    Stack,
-    Textarea,
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Select,
+  Stack,
+  Tooltip,
 } from "@chakra-ui/react";
+import { AiTwotoneBulb } from "react-icons/ai";
 import React from "react";
 
 const AddStaffDrawer = (props) => {
-    return (
-        <>
-            <Drawer
-                isOpen={props.isOpen}
-                placement="right"
-                onClose={props.onClose}
-                size="md"
-            >
-                <DrawerOverlay />
-                <DrawerContent>
-                    <DrawerCloseButton />
-                    <DrawerHeader borderBottomWidth="1px">Add New Tables</DrawerHeader>
+  return (
+    <>
+      <Drawer
+        isOpen={props.isOpen}
+        placement="right"
+        onClose={props.onClose}
+        size="md"
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth="1px">Add Staff Member</DrawerHeader>
 
-                    <DrawerBody>
-                        <Stack spacing="24px">
-                            <Box m={6}>
-                                <FormControl mt={5}>
-                                    <FormLabel>
-                                        How would you like to create new tables?
-                                    </FormLabel>
-                                    <RadioGroup defaultValue="Itachi">
-                                        <HStack spacing="24px">
-                                            <Radio value="Automatic Table Names">
-                                                Automatic Table Names
-                                            </Radio>
-                                            <Radio value="Custom Table Names">
-                                                Custom Table Names
-                                            </Radio>
-                                        </HStack>
-                                    </RadioGroup>
-                                </FormControl>
+          <DrawerBody>
+            <Stack spacing="24px">
+              <Box m={6}>
+                <FormControl mt={5}>
+                  <FormLabel fontWeight="400">ID</FormLabel>
+                  <Input placeholder="ID" size="md" borderRadius="8px" />
+                </FormControl>
 
-                                <FormControl mt={5}>
-                                    <FormLabel fontWeight="400">Label (Suffix):</FormLabel>
-                                    <Input placeholder="" size="sm" borderRadius="8px" />
-                                </FormControl>
+                <FormControl mt={5}>
+                  <FormLabel fontWeight="400">First Name</FormLabel>
+                  <Input
+                    placeholder="First Name"
+                    size="md"
+                    borderRadius="8px"
+                  />
+                </FormControl>
 
-                                <FormControl mt={5}>
-                                    <FormLabel fontWeight="400">Number of Tables:</FormLabel>
-                                    <NumberInput min={1} size="sm">
-                                        <NumberInputField borderRadius="8px" />
-                                        <NumberInputStepper>
-                                            <NumberIncrementStepper />
-                                            <NumberDecrementStepper />
-                                        </NumberInputStepper>
-                                    </NumberInput>
-                                </FormControl>
+                <FormControl mt={5}>
+                  <FormLabel fontWeight="400">Last Name</FormLabel>
+                  <Input placeholder="Last Name" size="md" borderRadius="8px" />
+                </FormControl>
 
-                                <FormControl mt={3}>
-                                    <FormLabel fontWeight="400">Starting from:</FormLabel>
-                                    <Input placeholder="1" size="sm" borderRadius="8px" />
-                                </FormControl>
-                            </Box>
-                        </Stack>
-                    </DrawerBody>
-
-                    <DrawerFooter borderTopWidth="1px">
-                        <Button variant="outline" mr={3} onClick={props.onClose}>
-                            Cancel
+                <FormControl mt={5}>
+                  <FormLabel fontWeight="400">Pincode</FormLabel>
+                  <InputGroup>
+                    <Input pr="1.5rem" borderRadius="6" />
+                    <InputRightElement width="4.5rem">
+                      <Tooltip label="Generate" placement="top">
+                        <Button h="1.75rem" size="sm">
+                          <AiTwotoneBulb />
                         </Button>
-                        <Button colorScheme="blue">Create Tables</Button>
-                    </DrawerFooter>
-                </DrawerContent>
-            </Drawer>
-        </>
-    )
-}
+                      </Tooltip>
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
 
-export default AddStaffDrawer
+                <FormControl mt={5}>
+                  <FormLabel fontWeight="400">Check Name</FormLabel>
+                  <Input
+                    placeholder="Check Name"
+                    size="md"
+                    borderRadius="8px"
+                  />
+                </FormControl>
+
+                <FormControl mt={5}>
+                  <FormLabel fontWeight="400">Status</FormLabel>
+                  <Select size="md" borderRadius="8px">
+                    <option>Active</option>
+                    <option>Deactive</option>
+                  </Select>
+                </FormControl>
+              </Box>
+            </Stack>
+          </DrawerBody>
+
+          <DrawerFooter borderTopWidth="1px">
+            <Button variant="outline" mr={3} onClick={props.onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Create Tables</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default AddStaffDrawer;
