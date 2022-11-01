41dbbe5562d60bc5d4b205d573cf4c325b5b8c91
faizan-99 <mfaizy605@gmail.com>
2022-11-01 17:19:44 +0500

    section drawer done

--- src/components/MenuManagement/SectionDrawer.js
+++ src/components/MenuManagement/SectionDrawer.js
@@ -13,8 +13,19 @@ import {
   TabPanels,
   Tab,
   TabPanel,
+  FormControl,
+  FormLabel,
+  Textarea,
+  Switch,
+  Checkbox,
+  Select,
+  RadioGroup,
+  Stack,
+  Radio,
+  HStack,
 } from "@chakra-ui/react";
 import React from "react";
+import { BsFillGridFill, BsListTask } from "react-icons/bs";
 
 const SectionDrawer = (props) => {
   return (
@@ -39,16 +50,75 @@ const SectionDrawer = (props) => {
 
               <TabPanels>
                 <TabPanel>
-                  <p>one!</p>
+                  <FormControl>
+                    <FormLabel fontWeight="400">Name</FormLabel>
+                    <Input type="text" />
+                  </FormControl>
+
+                  <FormControl mt={3}>
+                    <FormLabel fontWeight="400">Description</FormLabel>
+                    <Textarea placeholder="Here is a sample placeholder" />
+                  </FormControl>
+
+                  <FormControl mt={3}>
+                    <FormLabel fontWeight="400">Note</FormLabel>
+                    <Input type="text" />
+                  </FormControl>
+
+                  <FormControl mt={3}>
+                    <FormLabel fontWeight="400">Display the section</FormLabel>
+                    <Switch />
+                  </FormControl>
+
+                  <FormControl mt={3}>
+                    <Checkbox defaultChecked>Use as a sub-section</Checkbox>
+                    <Input type="text" mt={2} />
+                  </FormControl>
                 </TabPanel>
                 <TabPanel>
-                  <p>two!</p>
+                  <FormControl>
+                    <FormLabel fontWeight="400">Labels</FormLabel>
+                    <Select>
+                      <option>New</option>
+                      <option>Signature</option>
+                    </Select>
+                  </FormControl>
+
+                  <FormControl mt={5}>
+                    <FormLabel fontWeight="400">Alphabetical Order</FormLabel>
+                    <Switch />
+                  </FormControl>
+
+                  <RadioGroup mt={5}>
+                    <Stack direction="row">
+                      <Radio value="1">List</Radio>
+                      <Radio value="2">Grid</Radio>
+                    </Stack>
+                  </RadioGroup>
+
+                  <FormControl mt={5}>
+                    <FormLabel fontWeight="400">Number of Columns</FormLabel>
+                    <Select>
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                    </Select>
+                  </FormControl>
+
+                  <FormControl mt={5}>
+                    <FormLabel fontWeight="400">Hide grid titles</FormLabel>
+                    <Switch />
+                  </FormControl>
                 </TabPanel>
               </TabPanels>
             </Tabs>
           </DrawerBody>
 
           <DrawerFooter>
+            <Checkbox defaultChecked mr="28%">
+              Save and add more
+            </Checkbox>
             <Button variant="outline" mr={3} onClick={props.onClose}>
               Cancel
             </Button>
