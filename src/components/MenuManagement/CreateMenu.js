c27a13e26799276f0eb51f7d7b649850502db9b1
faizan-99 <mfaizy605@gmail.com>
2022-11-01 14:21:25 +0500

    updated add button

--- src/components/MenuManagement/CreateMenu.js
+++ src/components/MenuManagement/CreateMenu.js
@@ -1,7 +1,61 @@
+import {
+  Box,
+  Button,
+  Grid,
+  GridItem,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Portal,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
+import { BsPlusLg } from "react-icons/bs";
 
 const CreateMenu = () => {
-  return <div>CreateMenu</div>;
+  return (
+    <>
+      <Grid
+        h="200px"
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(5, 1fr)"
+        gap={4}
+        m={10}
+      >
+        <GridItem rowSpan={2} colSpan={1} bg="white" borderRadius={6}>
+          <Grid templateColumns="repeat(5, 1fr)" gap={4} p={5}>
+            <GridItem colSpan={2}>Section</GridItem>
+            <GridItem colStart={4} colEnd={6}></GridItem>
+          </Grid>
+        </GridItem>
+        <GridItem colSpan={4}>
+          <Box bg="white" borderRadius={6} display="flex" p={5}>
+            <VStack>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  colorScheme="teal"
+                  size="sm"
+                  variant="outline"
+                  leftIcon={<BsPlusLg />}
+                >
+                  Add
+                </MenuButton>
+                <Portal>
+                  <MenuList>
+                    <MenuItem>Section</MenuItem>
+                    <MenuItem>Item</MenuItem>
+                  </MenuList>
+                </Portal>
+              </Menu>
+            </VStack>
+          </Box>
+        </GridItem>
+      </Grid>
+    </>
+  );
 };
 
 export default CreateMenu;
