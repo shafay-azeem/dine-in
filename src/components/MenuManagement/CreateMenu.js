2905d5ce66ebfc800a8d4d4e3158dc7fab6d15f8
faizan-99 <mfaizy605@gmail.com>
2022-11-04 16:09:06 +0500

    updated

--- src/components/MenuManagement/CreateMenu.js
+++ src/components/MenuManagement/CreateMenu.js
@@ -1,19 +1,28 @@
+import { CheckIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
   Grid,
   GridItem,
   HStack,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
   Portal,
+  SimpleGrid,
+  Switch,
+  Text,
   useDisclosure,
   VStack,
 } from "@chakra-ui/react";
 import React from "react";
-import { BsPlusLg } from "react-icons/bs";
+import { BsPlusLg, BsThreeDotsVertical } from "react-icons/bs";
 import ItemDrawer from "./ItemDrawer";
 import SectionDrawer from "./SectionDrawer";
 
@@ -87,6 +96,69 @@ const CreateMenu = () => {
                 </Portal>
               </Menu>
             </VStack>
+          </Box>
+
+          <Box bg="white" borderRadius={6} p={5} mt={3}>
+            <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+              <GridItem colSpan={2}>
+                <HStack>
+                  <Image
+                    boxSize="60px"
+                    objectFit="cover"
+                    src="https://bit.ly/dan-abramov"
+                    alt="Dan Abramov"
+                  />
+                  <Text pl={2}>Your Section Name</Text>
+                </HStack>
+              </GridItem>
+              <GridItem colStart={4} colEnd={6}>
+                <HStack>
+                  <Switch p={5} pl="55%" />
+                  <Button colorScheme="teal" size="sm">
+                    Show
+                  </Button>
+                </HStack>
+              </GridItem>
+            </Grid>
+          </Box>
+
+          <Box bg="white" borderRadius={6} p={5} mt={3}>
+            <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+              <GridItem colSpan={2}>
+                <HStack>
+                  <Image
+                    boxSize="60px"
+                    objectFit="cover"
+                    src="https://bit.ly/dan-abramov"
+                    alt="Dan Abramov"
+                  />
+                  <Text pl={2}>Your Section Name</Text>
+                </HStack>
+              </GridItem>
+              <GridItem colStart={4} colEnd={6}>
+                <HStack>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      color="gray.500"
+                      fontSize="1.2em"
+                      children="$"
+                    />
+                    <Input placeholder="Enter amount" />
+                  </InputGroup>
+                  <Switch p={5} />
+                  <Menu>
+                    <MenuButton>
+                      <BsThreeDotsVertical as={Button} />
+                    </MenuButton>
+                    <MenuList>
+                      <MenuItem>Duplicate</MenuItem>
+                      <MenuItem>Delete</MenuItem>
+                    </MenuList>
+                  </Menu>
+                </HStack>
+              </GridItem>
+            </Grid>
           </Box>
         </GridItem>
       </Grid>
