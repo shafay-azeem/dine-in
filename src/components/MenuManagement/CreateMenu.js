f36586ddd12ba47886208cd0c6e7c5744300a809
faizan-99 <mfaizy605@gmail.com>
2022-11-07 12:51:37 +0500

    updated

--- src/components/MenuManagement/CreateMenu.js
+++ src/components/MenuManagement/CreateMenu.js
@@ -50,16 +50,33 @@ const CreateMenu = () => {
     setShow(true);
   }
 
+  const data = [
+    {
+      id: 1,
+      menuId: 101,
+      name: "Alfredo Paste",
+    },
+    {
+      id: 2,
+      menuId: 102,
+      name: "Sushi",
+    },
+    {
+      id: 3,
+      menuId: 103,
+      name: "Zinger ",
+    },
+  ];
+
   return (
     <>
       <Grid
-        h="200px"
         templateRows="repeat(2, 1fr)"
         templateColumns="repeat(5, 1fr)"
         gap={4}
         m={10}
       >
-        <GridItem rowSpan={2} colSpan={1} bg="white" borderRadius={6} h="180%">
+        <GridItem rowSpan={2} colSpan={1} bg="white" borderRadius={6} h="20%">
           <Grid templateColumns="repeat(5, 1fr)" gap={4} p={5}>
             <GridItem colSpan={2}>Section</GridItem>
             <GridItem colStart={4} colEnd={6}></GridItem>
@@ -111,35 +128,37 @@ const CreateMenu = () => {
             </VStack>
           </Box>
 
-          <Box bg="white" borderRadius={6} p={5} mt={3}>
-            <Grid templateColumns="repeat(5, 1fr)" gap={4}>
-              <GridItem colSpan={2}>
-                <HStack>
-                  <Image
-                    boxSize="60px"
-                    objectFit="cover"
-                    src="https://bit.ly/dan-abramov"
-                    alt="Dan Abramov"
-                  />
-                  <Text pl={2}>Your Section Name</Text>
-                </HStack>
-              </GridItem>
-              <GridItem colStart={4} colEnd={6}>
-                <HStack>
-                  <Switch p={5} pl="55%" />
-                  {show ? (
-                    <Button colorScheme="teal" size="sm" onClick={test}>
-                      Show
-                    </Button>
-                  ) : (
-                    <Button colorScheme="teal" size="sm" onClick={test2}>
-                      Hide
-                    </Button>
-                  )}
-                </HStack>
-              </GridItem>
-            </Grid>
-          </Box>
+          {data.map((x) => (
+            <Box bg="white" borderRadius={6} p={5} mt={3} key={x.menuId}>
+              <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+                <GridItem colSpan={2}>
+                  <HStack>
+                    <Image
+                      boxSize="60px"
+                      objectFit="cover"
+                      src="https://bit.ly/dan-abramov"
+                      alt="Dan Abramov"
+                    />
+                    <Text pl={2}>{x.name}</Text>
+                  </HStack>
+                </GridItem>
+                <GridItem colStart={4} colEnd={6}>
+                  <HStack>
+                    <Switch p={5} pl="55%" />
+                    {show ? (
+                      <Button colorScheme="teal" size="sm" onClick={test}>
+                        Show
+                      </Button>
+                    ) : (
+                      <Button colorScheme="teal" size="sm" onClick={test2}>
+                        Hide
+                      </Button>
+                    )}
+                  </HStack>
+                </GridItem>
+              </Grid>
+            </Box>
+          ))}
 
           {toggle ? (
             <Box bg="white" borderRadius={6} p={5} mt={3}>
