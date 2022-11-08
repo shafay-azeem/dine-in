e6e15f9340b149abeb58008656629e8430e067f1
shafay-azeem <shafayazeem12@gmail.com>
2022-11-08 19:46:02 +0500

    updated

--- src/components/Partials/MenuCards/DisplayCard.js
+++ src/components/Partials/MenuCards/DisplayCard.js
@@ -18,48 +18,83 @@ import CustomButton from "../../../CustomElements/CustomButton";
 import { AiTwotoneEdit } from "react-icons/ai";
 import { AiFillSetting } from "react-icons/ai";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import { useState } from "react";
 
 const DisplayCard = () => {
+
+  const [data, setData] = useState([
+    {
+      id: 0,
+      menuId: 101,
+      name: "MENU 1",
+      status: "LIVE",
+      item: 1,
+      date: "09-2-22",
+      active: false,
+    },
+    {
+      id: 0,
+      menuId: 101,
+      name: "MENU 2",
+      status: "OFFLINE",
+      item: 0,
+      date: "11-2-22",
+      active: false,
+    },
+    {
+      id: 0,
+      menuId: 101,
+      name: "MENU 3",
+      status: "IDLE",
+      item: 1,
+      date: "06-2-22",
+      active: false,
+    },
+  ]);
   return (
     <>
-      <Box bg="white" w="100%" p={4} borderRadius={5}>
-        <Grid templateColumns="repeat(5, 1fr)" gap={4}>
-          <GridItem colSpan={2}>
-            <Text fontSize="20" fontWeight="500">
-              Menu
-              <Badge
-                ml="2"
-                colorScheme="green"
-                borderRadius={5}
-                fontWeight="500"
-                fontSize="11"
-              >
-                Live
-              </Badge>
-            </Text>
-          </GridItem>
-          <GridItem colStart={4} colEnd={6}>
-            <HStack mt={2} gap={4} ml="38%">
-              <Switch />
-              <CustomButton
-                size={"sm"}
-                btnText={"Edit Menu"}
-                leftIcon={<AiTwotoneEdit />}
-              />
-              <AiFillSetting />
-              <Menu>
-                <MenuButton>
-                  <BsThreeDotsVertical as={Button} />
-                </MenuButton>
-                <MenuList>
-                  <MenuItem>Duplicate</MenuItem>
-                  <MenuItem>Delete</MenuItem>
-                </MenuList>
-              </Menu>
-            </HStack>
-          </GridItem>
-        </Grid>
-      </Box>
+      {data.map((x, index) => (
+        <Box bg="white" w="100%" p={4} borderRadius={5} mb={4}>
+          <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+            <GridItem colSpan={2}>
+              <Text fontSize="17" fontWeight="500">
+                {x.name}
+                <Badge
+                  ml="2"
+                  colorScheme="green"
+                  borderRadius={5}
+                  fontWeight="500"
+                  fontSize="11"
+                >
+                  {x.status}
+                </Badge>
+              </Text>
+
+            </GridItem>
+            <GridItem colStart={4} colEnd={6}>
+              <HStack mt={2} gap={4} ml="38%">
+                <Switch />
+                <CustomButton
+                  size={"sm"}
+                  btnText={"Edit Menu"}
+                  leftIcon={<AiTwotoneEdit />}
+                />
+                <AiFillSetting />
+                <Menu>
+                  <MenuButton>
+                    <BsThreeDotsVertical as={Button} />
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem>Duplicate</MenuItem>
+                    <MenuItem>Delete</MenuItem>
+                  </MenuList>
+                </Menu>
+              </HStack>
+            </GridItem>
+          </Grid>
+        </Box>
+      ))}
+
     </>
   );
 };
