ded8ac43b50638f550a2faa955d7c3fb4e1cfaab
shafay-azeem <shafayazeem12@gmail.com>
2022-11-14 15:00:42 +0500

    updated

--- src/components/Partials/MenuCards/SectionCard.js
+++ src/components/Partials/MenuCards/SectionCard.js
@@ -14,49 +14,89 @@ import { useState } from "react";
 import ItemCard from "./ItemCard";
 import { AiOutlineDown } from "react-icons/ai";
 import { MenuState } from "../../../context/MenuContext";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const SectionCard = (props) => {
   const { section, setSection, menu, setMenu } = MenuState();
-  console.log(section);
+
+
+  const [sectionList, setSectionList] = useState(section);
 
   function sectionClick(index) {
     section[index].itemActive = !section[index].itemActive;
     setSection([...section]);
-    console.log(section, "menu in sectiom");
   }
+
+  const handleDrop = (droppedItem) => {
+
+    if (!droppedItem.destination) return;
+    var updatedList = [...sectionList];
+
+    const [reorderedItem] = updatedList.splice(droppedItem.source.index, 1);
+
+    updatedList.splice(droppedItem.destination.index, 0, reorderedItem);
+
+    setSectionList(updatedList);
+    setSection(updatedList)
+  };
 
   return (
     <>
-      {section.map((x, index) => (
-        <Box bg="white" borderRadius={6} p={5} mt={3} key={index}>
-          <Grid templateColumns="repeat(5, 1fr)" gap={4}>
-            <GridItem colSpan={2}>
-              <HStack>
-                <Image
-                  boxSize="60px"
-                  objectFit="cover"
-                  src="https://bit.ly/dan-abramov"
-                  alt="Dan Abramov"
-                />
-                <Text pl={2}>{x.sectionName}</Text>
-              </HStack>
-            </GridItem>
-            <GridItem colStart={4} colEnd={6}>
-              <HStack>
-                <Switch p={5} pl="55%" />
-                <AiOutlineDown onClick={() => sectionClick(index)} />
-                {/* <Button
+      <DragDropContext onDragEnd={handleDrop}>
+        <Droppable droppableId="droppable-1">
+          {(provided) => (
+            <Box {...provided.droppableProps} ref={provided.innerRef} >
+              {section.map((x, index) => {
+                return (
+                  <Draggable key={x.sectionName} draggableId={x.sectionName} index={index}>
+                    {(provided) => (
+
+                      <Box bg="white" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} borderRadius={6} p={5} mt={3} key={index}>
+                        <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+                          <GridItem colSpan={2}>
+                            <HStack>
+                              <Image
+                                boxSize="60px"
+                                objectFit="cover"
+                                src="https://bit.ly/dan-abramov"
+                                alt="Dan Abramov"
+                              />
+                              <Text pl={2}>{x.sectionName}</Text>
+                            </HStack>
+                          </GridItem>
+                          <GridItem colStart={4} colEnd={6}>
+                            <HStack>
+                              <Switch p={5} pl="55%" />
+                              <AiOutlineDown onClick={() => sectionClick(index)} />
+                              {/* <Button
                   colorScheme="teal"
                   size="sm"
                   onClick={() => sectionClick(index)}
                 ></Button> */}
-              </HStack>
-            </GridItem>
-          </Grid>
+                            </HStack>
+                          </GridItem>
+                        </Grid>
 
-          {x.itemActive ? <ItemCard /> : console.log("false sss")}
-        </Box>
-      ))}
+                        {x.itemActive ? <ItemCard /> : console.log("false sss")}
+                      </Box>
+
+
+                    )}
+
+
+
+
+                  </Draggable>
+                )
+              })
+              }
+              {provided.placeholder}
+            </Box>
+
+          )}
+        </Droppable>
+      </DragDropContext>
+
     </>
   );
 };
