23df80078f942e6c07fad6cb05dbb4bf7a550a8b
faizan-99 <mfaizy605@gmail.com>
2022-12-07 16:31:23 +0500

    updated

--- src/components/RestaurantMenu/MenuDetail.js
+++ src/components/RestaurantMenu/MenuDetail.js
@@ -18,95 +18,66 @@ const MenuDetail = (props) => {
 
   let itemDetailResponse =
     response[menu_index]?.section[section_index]?.item[item_index];
-  //console.log(itemDetailResponse, "itemDetailResponse");
-  // itemDetailResponse &&
-  //   Object.keys(itemDetailResponse)?.map((itemDetailResponse) =>
-  //     console.log(itemDetailResponse, "itemDetailResponse")
-  //   );
+  //console.log([itemDetailResponse], "itemDetailResponse");
 
-  //const convertObj = Object.keys(itemDetailResponse);
-  //console.log(convertObj, "convertObj");
-
-  // const convertResp = Object.keys(itemDetailResponse);
-  // console.log(convertResp, "convert obj");
-
-  //let convertedResponse = Object.values(itemDetailResponse);
-  //console.log(convertedResponse, "convertedResponse");
+  let itemPriceOptionResponse =
+    response[menu_index]?.section[section_index]?.item[item_index]
+      .itemPriceOption;
+  //console.log(itemPriceOptionResponse, "itemPriceOptionResponse");
 
   return (
     <div>
       <Container className="mt-5">
         <Row>
           <Col lg={12} md={12} sm={12} xs={12}>
-            <Card className="mx-auto mb-1">
-              <Card.Body>
-                <Row>
-                  <Col lg={4} className="p-0 d-flex align-items-center">
-                    <img
-                      src={require("../Assets/burger.jpg")}
-                      className="image mx-auto d-block w-100 align-items-center "
-                    />
-                  </Col>
-                  <Col lg={8}>
-                    <Card.Title className="d-flex align-items-center justify-content-center mt-3">
-                      x.itemName
-                    </Card.Title>
+            {[itemDetailResponse]?.map((x) => {
+              return (
+                <Card className="mx-auto mb-1">
+                  <Card.Body>
+                    <Row>
+                      <Col lg={4} className="p-0 d-flex align-items-center">
+                        <img
+                          src={require("../Assets/burger.jpg")}
+                          className="image mx-auto d-block w-100 align-items-center "
+                        />
+                      </Col>
+                      <Col lg={8}>
+                        <Card.Title className="d-flex align-items-center justify-content-center mt-3">
+                          {x.itemName}
+                        </Card.Title>
 
-                    <Card.Text className="d-flex align-items-center justify-content-center">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </Card.Text>
-                    <Card.Text className="pricetext">$795.00</Card.Text>
-                    <div class=" my-4">
-                      <Row className="align-items-start">
-                        <Col lg={8}>
-                          <Card.Text className="d-flex align-items-start justify-content-start">
-                            Start aligned text on all viewport sizes
-                          </Card.Text>
-                        </Col>
-                        <Col lg={4}>
-                          <Card.Text className="pricetext d-flex align-items-end justify-content-end">
-                            $795.00
-                          </Card.Text>
-                        </Col>
-                      </Row>
-                      <hr className="dashed" />
-                    </div>
+                        <Card.Text className="d-flex align-items-center justify-content-center">
+                          {x.itemDescription}
+                        </Card.Text>
+                        <Card.Text className="pricetext">
+                          ${x.itemPrice}
+                        </Card.Text>
 
-                    <div class=" my-4">
-                      <Row className="align-items-start">
-                        <Col lg={8}>
-                          <Card.Text className="d-flex align-items-start justify-content-start">
-                            Start aligned text on all viewport sizes
-                          </Card.Text>
-                        </Col>
-                        <Col lg={4}>
-                          <Card.Text className="pricetext d-flex align-items-end justify-content-end">
-                            $795.00
-                          </Card.Text>
-                        </Col>
-                      </Row>
-                      <hr className="dashed" />
-                    </div>
-                    <div class=" my-4">
-                      <Row className="align-items-start">
-                        <Col lg={8}>
-                          <Card.Text className="d-flex align-items-start justify-content-start">
-                            Start aligned text on all viewport sizes
-                          </Card.Text>
-                        </Col>
-                        <Col lg={4}>
-                          <Card.Text className="pricetext d-flex align-items-end justify-content-end">
-                            $795.00
-                          </Card.Text>
-                        </Col>
-                      </Row>
-                      <hr className="dashed" />
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
+                        {itemPriceOptionResponse?.map((y) => {
+                          return (
+                            <div class=" my-4">
+                              <Row className="align-items-start">
+                                <Col lg={8}>
+                                  <Card.Text className="d-flex align-items-start justify-content-start">
+                                    {y.name}
+                                  </Card.Text>
+                                </Col>
+                                <Col lg={4}>
+                                  <Card.Text className="pricetext d-flex align-items-end justify-content-end">
+                                    $ {y.price}
+                                  </Card.Text>
+                                </Col>
+                              </Row>
+                              <hr className="dashed" />
+                            </div>
+                          );
+                        })}
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                </Card>
+              );
+            })}
           </Col>
         </Row>
       </Container>
