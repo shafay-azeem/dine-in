a43a3b380eaa3e7fdc112e519b4ee4bd8d0f4586
shafay-azeem <shafayazeem12@gmail.com>
2023-02-28 22:14:26 +0500

    updated

--- src/components/Partials/Pagination.js
+++ src/components/Partials/Pagination.js
@@ -2,8 +2,16 @@ import React from "react";
 import _ from "lodash";
 
 const Pagination = ({ total, currentPage, perPage, onPageChange }) => {
-  const totalPages = Math.ceil(total / perPage);
+  let totalPages = Math.ceil(total / perPage);
+  console.log(total, 'total')
+  if (totalPages == 0) {
+    totalPages = 1
+    currentPage = 1
+  }
+
   const pages = _.range(1, totalPages + 1);
+  console.log(pages)
+
   return (
     <nav>
       <ul
@@ -56,66 +64,6 @@ const Pagination = ({ total, currentPage, perPage, onPageChange }) => {
         </li>
       </ul>
     </nav>
-
-    // <nav>
-    //   <ul
-    //     className="pagination"
-    //     style={{
-    //       display: "flex",
-    //       justifyContent: "center",
-    //       alignItems: "center",
-    //     }}
-    //   >
-    //     <li
-    //       key="previous"
-    //       className={currentPage === 1 ? "page-item disabled" : "page-item"}
-    //     >
-    //       <button
-    //         className="page-link"
-    //         onClick={() => onPageChange(currentPage - 1)}
-    //         disabled={currentPage === 1}
-    //         aria-label="Previous"
-    //       >
-    //         <span aria-hidden="true">&laquo;</span>
-    //         <span className="sr-only">Previous</span>
-    //       </button>
-    //     </li>
-    //     {pages
-    //       .filter((page) => {
-    //         return page >= currentPage - 2 && page <= currentPage + 2;
-    //       })
-    //       .map((page) => (
-    //         <li
-    //           key={page}
-    //           className={
-    //             page === currentPage ? "page-item active" : "page-item"
-    //           }
-    //         >
-    //           <button className="page-link" onClick={() => onPageChange(page)}>
-    //             {page}
-    //           </button>
-    //         </li>
-    //       ))}
-    //     {currentPage < totalPages && (
-    //       <li
-    //         key="next"
-    //         className={
-    //           currentPage === totalPages ? "page-item disabled" : "page-item"
-    //         }
-    //       >
-    //         <button
-    //           className="page-link"
-    //           onClick={() => onPageChange(currentPage + 1)}
-    //           disabled={currentPage === totalPages}
-    //           aria-label="Next"
-    //         >
-    //           <span aria-hidden="true">&raquo;</span>
-    //           <span className="sr-only">Next</span>
-    //         </button>
-    //       </li>
-    //     )}
-    //   </ul>
-    // </nav>
   );
 };
 
